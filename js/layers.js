7d442dba14f11374b56ce914d81d1d356390c69b
Zenith-maker <Jonathan2009c@gmail.com>
2026-06-19 17:47:08 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("p", {
-    name: "Addition", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "Shards", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,14 +8,13 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Addites", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Shards", // Name of prestige currency
+    baseResource: "Fragments", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -26,31 +25,32 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     upgrades: {
+        
         11: {
-        Title: "Addition Boost",
-        description:  "Multiples point gain by 2",
-        cost: new Decimal(1),
+        Title: "Who am I?",
+        description: "Fragment gain is multiplied by 2",
+        cost: new Decimal(1)
         },
-        12: {
-            Title: "Even Larger Boost",
-            description:  "Prestige Points Boost point gain",
-            cost: new Decimal(2),
-            effect() {
-                return player[this.layer].points.add(1).pow(2)
-            },
-            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
-        
-            },
-        13: {
-                Title: "Even Larger Boost",
-                description:  "Prestige Points Boost point gain",
-                cost: new Decimal(5),
-                effect() {
-                    return player.points.add(1).pow(0.15)
-                },
-                },
 
-        
+        12: {
+        Title: "Who am I?",
+        description: "Fragment gain is multiplied by Shards",
+        cost: new Decimal(1),
+        effect() {
+            return player[this.layer].points.add(1).pow(0.5)
+        },
+        effectDisplay() { return format(upgradeEffect("p" , 12))+"x" },
+        },
+
+    },
+    buyables: {
+        cost(x) { return new Decimal(1).mul(x) },
+        display() { return "Blah" },
+        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        buy() {
+            player[this.layer].points = player[this.layer].points.sub(this.cost())
+            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+        },
     },
     layerShown(){return true}
 })
